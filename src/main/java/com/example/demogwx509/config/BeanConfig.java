2f9889c76c68c950838bae8208f381b2aa59bd16
donhee <saytokang@gmail.com>
2022-11-11 14:16:03 +0900

    사용하지 않는 빈 제거

--- src/main/java/com/example/demogwx509/config/BeanConfig.java
+++ src/main/java/com/example/demogwx509/config/BeanConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
@@ -20,25 +16,12 @@ import reactor.core.publisher.Mono;
 public class BeanConfig {
 
 	@Bean
-	public ReactiveUserDetailsService reactiveUserDetailsService() {
-		// @formatter:off
-		UserDetails client = User.withUsername("client")
-			.password("")
-			.roles("USER")
-			.build();
-		// @formatter:on
-		return new MapReactiveUserDetailsService(client);
-	}
-
-	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		SubjectDnX509PrincipalExtractor principalExtractor = new SubjectDnX509PrincipalExtractor();
 		principalExtractor.setSubjectDnRegex("CN=(.*?)(?:,|$)");
 		ReactiveAuthenticationManager authenticationManager = authentication -> {
 			log.info("** OU: {}", authentication.getName());
 			authentication.setAuthenticated(true);
-			// authentication.setAuthenticated("Trusted Org
-			// Unit".equals(authentication.getName()));
 			return Mono.just(authentication);
 		};
 
