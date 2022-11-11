c6e15ec0092b90d6919a6d30201c8d82857e8492
donhee <saytokang@gmail.com>
2022-11-11 14:14:44 +0900

    x509 memory db  체크 기능

--- src/main/java/com/example/demogwx509/config/BeanConfig.java
+++ src/main/java/com/example/demogwx509/config/BeanConfig.java
@@ -1,16 +1,19 @@
 package com.example.demogwx509.config;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.preauth.x509.SubjectDnX509PrincipalExtractor;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+import org.springframework.web.server.ResponseStatusException;
 
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
@@ -19,27 +22,22 @@ import reactor.core.publisher.Mono;
 @Configuration
 public class BeanConfig {
 
-	@Bean
-	public ReactiveUserDetailsService reactiveUserDetailsService() {
-		// @formatter:off
-		UserDetails client = User.withUsername("client")
-			.password("")
-			.roles("USER")
-			.build();
-		// @formatter:on
-		return new MapReactiveUserDetailsService(client);
-	}
+	private String pattern = "CN=(.*?)(?:,|$)";
 
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		SubjectDnX509PrincipalExtractor principalExtractor = new SubjectDnX509PrincipalExtractor();
-		principalExtractor.setSubjectDnRegex("CN=(.*?)(?:,|$)");
+		principalExtractor.setSubjectDnRegex(pattern);
 		ReactiveAuthenticationManager authenticationManager = authentication -> {
 			log.info("** OU: {}", authentication.getName());
-			authentication.setAuthenticated(true);
-			// authentication.setAuthenticated("Trusted Org
-			// Unit".equals(authentication.getName()));
-			return Mono.just(authentication);
+
+			if (!dbQueue().contains(authentication.getName())) {
+				return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN, "not allowed"));
+			}
+			else {
+				authentication.setAuthenticated(true);
+				return Mono.just(authentication);
+			}
 		};
 
 		// @formatter:off
@@ -52,6 +50,12 @@ public class BeanConfig {
 
 		http.securityContextRepository(NoOpServerSecurityContextRepository.getInstance());
 		return http.build();
+	}
+
+	@Bean
+	public Queue<String> dbQueue() {
+		List<String> data = Arrays.asList("abc", "client", "bbbb");
+		return new ConcurrentLinkedQueue<>(data);
 	}
 
 }
